a44dd640e4d1988545cf2c14ee080c3da516ed6b
Andrii Pelesh <truepele@gmail.com>
2019-01-04 16:31:01 -0800

    RandomExtensions.NextIntEnumerable

---
+++ Tests/RandomExtensionsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using truepele.Common;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class RandomExtensionsTests
+    {
+        [TestCase(1)]
+        [TestCase(9)]
+        [TestCase(1000000)]
+        public void NextIntEnumerable_ReturnsCorrectCount(int count)
+        {
+            // Arrange
+            var r = new Random();
+
+            // Act 
+            var result = r.NextIntEnumerable(count);
+
+            // Assert
+            Assert.AreEqual(count, result.Count());
+        }
+
+        
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(9)]
+        [TestCase(22)]
+        [TestCase(1100)]
+        [TestCase(1105)]
+        [TestCase(1000000)]
+        public void NextIntEnumerable_ReturnsCorrectMaxValue(int maxValue)
+        {
+            // Arrange
+            var r = new Random();
+
+            // Act 
+            var result = r.NextIntEnumerable(100, maxValue);
+
+            // Assert
+            Assert.LessOrEqual(result.Max(), maxValue);
+        }
+
+
+        [Test]
+        public void NextIntEnumerable_Throws_ForNullInstance()
+        {
+            // Arrange
+            Random r = null;
+
+            // Act / Assert
+            Assert.Throws<ArgumentNullException>(() => r.NextIntEnumerable(100));
+        }
+
+
+        [Test]
+        public void NextIntEnumerable_DoeNotThrow_ForNullMaxValue()
+        {
+            // Arrange
+            var r = new Random();
+
+            // Act / Assert
+            Assert.DoesNotThrow(() => r.NextIntEnumerable(100, null));
+        }
+
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-1000000)]
+        public void NextIntEnumerable_Throws_ForCountLessThenOne(int count)
+        {
+            // Arrange
+            var r = new Random();
+
+            // Act / Assert
+            Assert.Throws<ArgumentException>(() => r.NextIntEnumerable(count));
+        }
+    }
+}
---
+++ misc/Common/RandomExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace truepele.Common
+{
+    public static class RandomExtensions
+    {
+        public static IEnumerable<int> NextIntEnumerable(this Random random, int count, int? maxValue = null)
+        {
+            if (random == null) throw new ArgumentNullException(nameof(random));
+
+            if (count < 1) throw new ArgumentException($"{nameof(count)}", nameof(count));
+
+            return Enumerable
+                .Range(0, count)
+                .Select(_ => maxValue.HasValue ? random.Next(maxValue.Value) : random.Next());
+        }
+    }
+}
